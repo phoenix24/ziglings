c2043c2e7ee6b5c259c274aef2496c89fd3ca90f
phoenix24 <phoenix24@users.noreply.github.com>
2025-12-16 01:01:48 +0530

    fixed exercise 14

--- exercises/014_while4.zig
+++ exercises/014_while4.zig
@@ -14,11 +14,11 @@ const std = @import("std");
 
 pub fn main() void {
     var n: u32 = 1;
-
+    
     // Oh dear! This while loop will go forever?!
     // Please fix this so the print statement below gives the desired output.
     while (true) : (n += 1) {
-        if (???) ???;
+        if (n == 4) break;
     }
 
     // Result: we want n=4
